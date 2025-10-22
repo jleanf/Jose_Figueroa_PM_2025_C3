47ba915aef107437a2b53aec693968cb5e1ae433
jleanf <joseleandrofg@gmail.com>
2025-10-21 23:29:01 -0400

    Tarea 5: ejemplos e investigaciones

---
+++ Tarea 5/Ejemplos Capitulo 7/Programa 7.8/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/* Minusculas y mayusculas.
+
+El programa, al recibir como dato un arreglo unidimensional de tipo
+cadena de caracteres, determina el numero de minusculas y mayusculas
+que hay en cada cadena. */
+
+void minymay(char *cad); /* Prototipo de función. Modificación "*" añadido*/
+
+void main(void)
+{
+    int i, n;
+    char FRA[20][50];
+    /* Observa cómo se declara el arreglo unidimensional de cadena de
+    caracteres. */
+    printf("\nIngrese el numero de filas del arreglo: ");
+    scanf("%d", &n);
+    getchar();
+    for (i=0; i<n; i++)
+    {
+        /* Para cada fila se lee la cadena correspondiente. */
+        printf("Ingrese la linea %d de texto: ", i+1);
+        //fflush(stdin); <--- Modificado (no funciona)
+        gets(FRA[i]);
+    }
+    printf("\n\n");
+    for (i=0; i<n; i++)
+    {
+        minymay(FRA[i]);
+    }
+}
+void minymay(char *cadena)
+/* Esta función se utiliza para calcular el numero de minusculas
+y mayusculas que hay en cada cadena. */
+{
+    int i = 0, mi = 0, ma = 0;
+    while(cadena[i] != '\0')
+    {
+        if (islower(cadena[i]))
+            mi++;
+        else if (isupper(cadena[i]))
+            ma++;
+        i++;
+    }
+    printf("\n\nNumero de letras minusculas: %d", mi);
+    printf("\nNumero de letras mayusculas: %d", ma);
+}
